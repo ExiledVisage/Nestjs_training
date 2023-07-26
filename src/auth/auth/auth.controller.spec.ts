71f65e5e1884ba8af0f9356a91cc413d47ec1ffe
ExiledVisage <kere12irmak@hotmail.com>
2023-07-26 13:30:36 +0300

    testing commit

--- src/auth/auth/auth.controller.spec.ts
+++ src/auth/auth/auth.controller.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Req } from '@nestjs/common';
+import * as request  from 'supertest';
+import { get } from 'http';
+import { application } from 'express';
+
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService
+
+  const request = require('supertest');
+  const assert = require('assert');
+  const express = require('express');
+  const app = express();
 
   beforeEach(async () => {
+    
+
+    app.get('/user', function(req, res) {
+      res.status(200).json({ name: 'Kerem' });
+    });
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getProfile', ()=> {
+    it('should do its job', async () => 
+    {
+      const result = ['test'];
+      jest.spyOn(controller, 'getProfile').mockImplementation(() => result);
+
+      expect(await controller.getProfile(Req)).toBe(result);
+    } )
+  })
+
+  describe('Sign in', function() {
+    it('responds with json', function(done) 
+    {
+      request(app)
+       .get('/users')
+       .auth('Kerem','020406')
+       .set('Accept','application/json')
+       .expect('Content-type', /json/)
+       .expect(200,done)       
+
+    })
+  })
 });
--- src/auth/auth/auth.service.spec.ts
+++ src/auth/auth/auth.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { INestApplication } from '@nestjs/common';
+import { AuthModule } from './auth.module';
+import { ModuleRef } from '@nestjs/core';
+import { get, request } from 'http';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
-
+      providers: [AuthService], imports: [AuthModule]
+    })    
+    .compile();    
     service = module.get<AuthService>(AuthService);
   });
+
+
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  
 });
